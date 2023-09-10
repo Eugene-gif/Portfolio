4c1c82fb73ff187b0987d13ae8b227e22ead47e0
Eugene <zuevevgen93@mail.ru>
2023-09-11 00:35:15 +0300

    Init

---
+++ Readme.md
@@ -0,0 +1,18 @@
+# [Портфолио](https://eugene-gif.github.io/Portfolio/#home)
+
+## Описание:
+
+### Инициализация
+```
+npm i (yarn)
+```
+
+### Запуск
+```
+npm run dev (yarn dev)
+```
+
+### Сборка для продакшена
+```
+npm run build (yarn build)
+```
---
+++ src/modules/menu.js
@@ -0,0 +1,33 @@
+const btn = document.querySelector('.header__menu-btn input');
+const overlay = document.querySelector('.overlay');
+const nav = document.querySelector('.nav');
+
+export function toggleMenu() {
+  overlay.classList.toggle('_show');
+  nav.classList.toggle('_show');
+  document.body.classList.toggle('_lock');
+  nav.querySelector('._active a').focus();
+}
+
+export function closeMenu() {
+  btn.checked = false;
+  overlay.classList.remove('_show');
+  nav.classList.remove('_show');
+  document.body.classList.remove('_lock');
+}
+
+btn.addEventListener('input', toggleMenu);
+overlay.addEventListener('click', closeMenu);
+document.addEventListener('keyup', (evt) => {
+  // if (evt.key === 'Escape' && btn.checked) {
+  //   closeMenu();
+  // }
+
+  if (evt.key === 'Escape') {
+    toggleMenu();
+    btn.checked = !btn.checked;
+  }
+})
+
+// export const toggleMenu = toggleMenu;
+// export const closeMenu = closeMenu;
